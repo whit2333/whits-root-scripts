abf3e7615b6f53ef4664ea6f1fbc9620bde02997
Whitney Armstrong <whit@temple.edu>
2013-03-20 13:54:55 -0400

    Added plots

--- plot_color_schemes.cxx
+++ plot_color_schemes.cxx
@@ -5,41 +5,48 @@
    col=3000;
    c = draw_colors(col);
    c->SaveAs(Form("plots/color_scheme%d.png",col));
-
+   c->SaveAs(Form("plots/color_scheme%d.svg",col));
 
    col=4000;
    c = draw_colors(col);
    c->SaveAs(Form("plots/color_scheme%d.png",col));
+   c->SaveAs(Form("plots/color_scheme%d.svg",col));
 
    //set_color_scheme(30,255,0,0,col=5000);
    col=5000;
    c = draw_colors(col);
    c->SaveAs(Form("plots/color_scheme%d.png",col));
+   c->SaveAs(Form("plots/color_scheme%d.svg",col));
 
    //set_color_scheme(45,55,0,100,col=6000);
    col=6000;
    c = draw_colors(col);
    c->SaveAs(Form("plots/color_scheme%d.png",col));
+   c->SaveAs(Form("plots/color_scheme%d.svg",col));
 
    //set_color_scheme(30,5,50,150,col=7000);
    col=7000;
    c = draw_colors(col);
    c->SaveAs(Form("plots/color_scheme%d.png",col));
+   c->SaveAs(Form("plots/color_scheme%d.svg",col));
 
    //set_color_scheme(60,0,30,50,col=8000);
    col=8000;
    c = draw_colors(col);
    c->SaveAs(Form("plots/color_scheme%d.png",col));
+   c->SaveAs(Form("plots/color_scheme%d.svg",col));
 
    //set_color_scheme(30,40,30,50,col=9000);
    col=9000;
    c = draw_colors(col);
    c->SaveAs(Form("plots/color_scheme%d.png",col));
+   c->SaveAs(Form("plots/color_scheme%d.svg",col));
 
    //set_color_scheme(23,100,50,0,col=10000);
    col=10000;
    c = draw_colors(col);
    c->SaveAs(Form("plots/color_scheme%d.png",col));
+   c->SaveAs(Form("plots/color_scheme%d.svg",col));
 
 }
 
